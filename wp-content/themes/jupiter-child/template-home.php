7df385f199aeab3f08b09d99f3e6cd9acd3c20c5
pozone <32913208+pozone@users.noreply.github.com>
2018-10-08 17:51:37 +0200

    Customizzato il menu e parte della homepage

---
+++ wp-content/themes/jupiter-child/template-home.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package jupiter
+ * Template Name: Homepage
+ *
+ */
+?>
+<?php get_header(); ?>
+
+<?php
+  ?>
+
+<div class="space_min"></div>
+
+<div class="row full-width">
+    aaaa
+</div>
+
+<?php get_footer(); ?>
